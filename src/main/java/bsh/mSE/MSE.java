472714932878b31431aeb029ee5c9489898f65a1
Sam Kirby <sam@brainstream.app>
2024-09-10 17:51:29 -0400

    initial commit

---
+++ src/main/java/bsh/mSE/MSE.java
@@ -0,0 +1,17 @@
+package bsh.mSE;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MSE extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
